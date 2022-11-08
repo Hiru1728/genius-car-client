85448d9eb88fa15ad871024b46ad0c33367ffab5
Hiru1728 <hirucse1728@gmail.com>
2022-11-09 03:26:26 +0600

    Genious car

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -4,13 +4,13 @@ import SevicesCard from './SevicesCard';
 const Services = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data))
 
     }, [])
     return (
-        <div>
+        <div className='my-5'>
             <div className='text-center mb-4'>
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
